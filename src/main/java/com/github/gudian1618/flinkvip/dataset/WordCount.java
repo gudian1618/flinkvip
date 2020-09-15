edb23b5441d8fd761bf6fcf0c3ff4f4e229efdce
gudian1618 <1761844211@qq.com>
2020-09-15 21:51:20 +0800

    new

--- src/main/java/com/github/gudian1618/flinkvip/dataset/WordCount.java
+++ src/main/java/com/github/gudian1618/flinkvip/dataset/WordCount.java
@@ -1,13 +1,27 @@
 package com.github.gudian1618.flinkvip.dataset;
 
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+
 /**
  * @author gudian1618
  * @version v1.0
  * @date 2020/9/15 9:41 下午
+ * 入门案例:单词统计
+ * hadoop hive flume kafka zookeeper hadoop flink flink
  */
 
 public class WordCount {
 
+    public static void main(String[] args) {
+        // 1.获取execution environment
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        // 2.加载/创建初始数据
+        DataSource<String> source = env.fromElements("hadoop hive flume kafka zookeeper hadoop flink flink");
+        //
+
+
+    }
 
 
 }
