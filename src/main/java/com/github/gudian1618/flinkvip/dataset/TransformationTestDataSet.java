6a480f188888ece21fe2d514fea436b340c88897
gudian1618 <1761844211@qq.com>
2020-09-23 22:43:21 +0800

    new

--- src/main/java/com/github/gudian1618/flinkvip/dataset/TransformationTestDataSet.java
+++ src/main/java/com/github/gudian1618/flinkvip/dataset/TransformationTestDataSet.java
@@ -1,13 +1,26 @@
 package com.github.gudian1618.flinkvip.dataset;
 
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+
 /**
  * @author gudian1618
  * @version v1.0
  * @date 2020/9/19 10:41 下午
+ * 针对Transformation练习
  */
 
 public class TransformationTestDataSet {
 
+    public static void main(String[] args) {
 
+        // 1.获取执行环境
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        // 2.获取数据源
+        DataSource<String> source = env.readTextFile("3.txt");
+        // 3.Transformation转化
+
+
+    }
 
 }
