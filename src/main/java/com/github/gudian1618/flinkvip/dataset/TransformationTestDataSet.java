82e3d3fcc4ec4f262e8fa6b39fbb0371a0d467e5
gudian1618 <1761844211@qq.com>
2020-09-28 23:08:38 +0800

    new

--- src/main/java/com/github/gudian1618/flinkvip/dataset/TransformationTestDataSet.java
+++ src/main/java/com/github/gudian1618/flinkvip/dataset/TransformationTestDataSet.java
@@ -19,17 +19,33 @@ public class TransformationTestDataSet {
         // 2.获取数据源
         // 可以通过控制并行度来进行计算
         // DataSource<String> source = env.readTextFile("src/main/java/com/github/gudian1618/flinkvip/dataset/3.txt").setParallelism(1);
-        // DataSource<String> source = env.fromElements("hadoop", "hive", "flume", "kafka", "flink");
+        DataSource<String> source = env.fromElements("hadoop", "hive", "flume", "kafka", "flink");
         // ================  ===========================
         // ================  ===========================
         // ================  ===========================
         // ================  ===========================
-
-        // ================ union:两个数据集中的数据的并集,要求两个数据字段必须一致 ===========================
         DataSource<String> source1 = env.fromElements("1|铁锤|22", "2|钢蛋|18", "3|琪琪|18");
-        DataSource<String> source2 = env.fromElements("铁锤|北京", "钢蛋|上海", "狗蛋|上海");
-        // 字段必须相同
-        source1.union(source2)
+        source1.first(2)
+        // DataSource<String> source2 = env.fromElements("铁锤|北京", "钢蛋|上海", "狗蛋|上海");
+        // MapOperator<String, Tuple3<String, String, String>> input1 = source1.map(new MapFunction<String, Tuple3<String, String, String>>() {
+        //     @Override
+        //     public Tuple3<String, String, String> map(String value) throws Exception {
+        //         String[] s = value.split("\\|");
+        //         return new Tuple3<>(s[0], s[1], s[2]);
+        //     }
+        // });
+        // MapOperator<String, Tuple2<String, String>> input2 = source2.map(new MapFunction<String, Tuple2<String, String>>() {
+        //     @Override
+        //     public Tuple2<String, String> map(String value) throws Exception {
+        //         String[] s = value.split("\\|");
+        //         return new Tuple2<>(s[0], s[1]);
+        //     }
+        // });
+        // ================ union:两个数据集中的数据的并集,要求两个数据字段必须一致 ===========================
+        // DataSource<String> source1 = env.fromElements("1|铁锤|22", "2|钢蛋|18", "3|琪琪|18");
+        // DataSource<String> source2 = env.fromElements("铁锤|北京", "钢蛋|上海", "狗蛋|上海");
+        // // 字段必须相同
+        // source1.union(source2)
         // ============== cross:笛卡尔积 =====================
         // DataSource<String> source1 = env.fromElements("1|铁锤|22", "2|钢蛋|18", "3|琪琪|18");
         // DataSource<String> source2 = env.fromElements("铁锤|北京", "钢蛋|上海", "狗蛋|上海");
